65b6246f711de99f62b0b059e9a025d1c804d3d3
ajq1 <ajq1@sfu.ca>
2025-05-26 15:46:20 -0700

    Add the re-named converter file

---
+++ src/converter.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]) {	
+	// Scrape the input file for it's name/original path
+	string inputFile = fs::absolute(argv[1]).string();
+	string inputFileName = fs::path(inputFile).stem().string();
+	
+	string appDirectory = fs::absolute(argv[0]).parent_path().string();
+	string exportDirectory = appDirectory + "/export/";
+	
+	// Fix some weird stuff with windows where if we drag a file in from a DIFFERENT directory, the program thinks we're running in that directory
+	string ffmpegExe = fs::absolute(appDirectory + "/tools/ffmpeg.exe").string();
+	
+	// Build the command we're gonna send to ffmpeg
+	#ifndef WAVIFY
+	string cmd = ffmpegExe + " -sample_fmt s16 -r 44100 -y -i \"" + inputFile + "\" -vn \"" + exportDirectory + inputFileName + ".wav\"";
+	#endif
+
+	#ifndef MP3IFY
+	string cmd = ffmpegExe + " -y -i \"" + inputFile + "\" -vn \"" + exportDirectory + inputFileName + ".mp3\"";
+	#endif
+
+	// Print the command, handy for debugging purposes
+	cout << cmd << "\n";
+	
+	// Execute the command
+	system(cmd.c_str());
+	return 0;
+}
